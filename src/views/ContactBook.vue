a0e089cc95c81dc6aa39986edfff17a96446d915
huydc24v7x312 <huydc24v7x312@dttx.ctu.edu.vn>
2025-11-05 21:21:36 +0700

    Update ContactBook.vue

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -17,7 +17,7 @@
 
         <div class="mt-3 row justify-content-around align-items-center">
             <button class="btn btn-sm btn-primary" @click="refreshList()">
-            <i class="fas fa-redo"></i> Làm mới
+                <i class="fas fa-refresh"></i> Làm mới
             </button>
             <button class="btn btn-sm btn-success" @click="goToAddContact">
                 <i class="fas fa-plus"></i> Thêm mới
@@ -26,7 +26,7 @@
                 class="btn btn-sm btn-danger"
                 @click="removeAllContacts"
             >
-                <i class="fas fa-trash"></i> Xóa tất cả
+                <i class="fas fa-trash-alt"></i> Xóa tất cả
             </button>
         </div>
     </div>
