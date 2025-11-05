4fcce03a3ef62c8d4f78ce9154e1a8f612002384
huydc24v7x312 <huydc24v7x312@dttx.ctu.edu.vn>
2025-11-05 21:20:43 +0700

    Update ContactCard.vue
    thêm liên hệ yêu thích và tình trang gia đình vào contactcard

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,7 +1,16 @@
 <script>
 export default {
+    name: "ContactCard",
     props: {
         contact: { type: Object, required: true },
+    },
+    data() {
+        return {
+            hobbies: ["Đọc sách", "Du lịch", "Nghe nhạc", "Thể thao"],
+            familyStatuses: ["Độc thân", "Đã kết hôn", "Ly hôn"],
+            selectedHobbies: [],
+            selectedStatus: "",
+        };
     },
 };
 </script>
@@ -26,8 +35,38 @@ export default {
     </div>
     <div class="p-1">
         <strong>Liên hệ yêu thích:&nbsp;</strong>
-        <i v-if="contact.favorite" class="fas fa-check"></i>
-        <i v-else class="fas fa-times"></i>
+        <i v-if="contact.favorite" class="fas fa-check-circle"></i>
+        <i v-else class="fas fa-times-circle"></i>
+    </div>
+      <!-- Sở thích (checkbox, chỉ xem) -->
+    <div class="p-1">
+      <strong>Sở thích:</strong><br />
+      <div class="form-check form-check-inline" v-for="(hobby, index) in hobbies" :key="index">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :id="'hobby' + index"
+          :checked="contact.hobbies && contact.hobbies.includes(hobby)"
+          disabled
+        />
+        <label class="form-check-label" :for="'hobby' + index">{{ hobby }}</label>
+      </div>
+    </div>
+
+    <!-- Tình trạng gia đình (radio, chỉ xem) -->
+    <div class="p-1">
+      <strong>Tình trạng gia đình:</strong><br />
+      <div class="form-check form-check-inline" v-for="(status, index) in familyStatuses" :key="index">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="familyStatus"
+          :id="'status' + index"
+          :checked="contact.familyStatus === status"
+          disabled
+        />
+        <label class="form-check-label" :for="'status' + index">{{ status }}</label>
+      </div>
     </div>
 </div>
 </template>
