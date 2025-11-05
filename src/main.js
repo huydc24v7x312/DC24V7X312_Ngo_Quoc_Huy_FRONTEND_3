369c761600507d87fba24aa05b5dbce3060be1c3
huydc24v7x312 <huydc24v7x312@dttx.ctu.edu.vn>
2025-11-05 21:21:29 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+//import "/node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import router from "./router";
-//createApp(App).mount('#app')
 createApp(App).use(router).mount("#app");
+
+import "@/assets/base.css";
+import "@/assets/main.css";
